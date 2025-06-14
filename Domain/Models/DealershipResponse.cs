7a49991b557b14ba7bcbc08a583268a9ec8418b1
Busher <besher.abodan@icloud.com>
2025-06-15 01:55:32 +0200

    Added API Skeleton Structure + Base Classes
    - Structure for Rest API
    - Packages:
    * Fluent Validation ASP.NET CORE
    * Entity Framework
    * AutoMapper
    * MailKit

---
+++ Domain/Models/DealershipResponse.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class DealershipResponse
+{
+    [Key]
+    public int ResponseId { get; set; }
+
+    [Required]
+    public int CarId { get; set; }
+
+    [ForeignKey(nameof(CarId))]
+    public Car Car { get; set; }
+
+    [Required]
+    public int DealershipId { get; set; }
+
+    [ForeignKey(nameof(DealershipId))]
+    public Dealership Dealership { get; set; }
+
+    [Required, MaxLength(200)]
+    [Column(TypeName = "nvarchar(200)")]
+    public string EmailSubject { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(MAX)")]
+    public string EmailBody { get; set; }
+
+    [Required, MaxLength(150)]
+    [Column(TypeName = "nvarchar(150)")]
+    public string FromEmail { get; set; }
+
+    [MaxLength(255)]
+    [Column(TypeName = "nvarchar(255)")]
+    public string MessageId { get; set; }
+
+    [MaxLength(255)]
+    [Column(TypeName = "nvarchar(255)")]
+    public string InReplyToMessageId { get; set; }
+
+    public DateTime ReceivedAt { get; set; } = DateTime.UtcNow;
+}
---
+++ Domain/Models/EmailLog.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class EmailLog
+{
+    [Key]
+    public int EmailLogId { get; set; }
+
+    [Required]
+    public int CarId { get; set; }
+
+    [ForeignKey(nameof(CarId))]
+    public Car Car { get; set; }
+
+    [Required]
+    public int DealershipId { get; set; }
+
+    [ForeignKey(nameof(DealershipId))]
+    public Dealership Dealership { get; set; }
+
+    [Required, MaxLength(150)]
+    [Column(TypeName = "nvarchar(150)")]
+    public string ToEmail { get; set; }
+
+    [Required, MaxLength(200)]
+    [Column(TypeName = "nvarchar(200)")]
+    public string Subject { get; set; }
+
+    [Required]
+    [Column(TypeName = "nvarchar(MAX)")]
+    public string Body { get; set; }
+
+    [MaxLength(255)]
+    [Column(TypeName = "nvarchar(255)")]
+    public string MessageId { get; set; }
+
+    public DateTime SentAt { get; set; } = DateTime.UtcNow;
+}
