359b667a8d4d2f2ef619074bc9af623f014c5105
Busher <besher.abodan@icloud.com>
2025-08-24 16:49:20 +0200

    redo

--- FE Page/vite.config.js
+++ FE Page/vite.config.js
@@ -4,9 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/CarDealership_FE/',
-  build: {
-    outDir: 'docs', // Change from 'dist' to 'docs'
-    emptyOutDir: true // Optional: cleans the docs folder before build
-  }
+  base: '/CarDealership_FE/'
 })
